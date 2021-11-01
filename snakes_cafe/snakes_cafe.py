db31195bd9a5c3f3ca8a695f2bf71a4fb1e88ce6
MohammadKhalil <moegts@gmail.com>
2021-11-01 14:46:39 +0200

    fix quit not working

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -66,37 +66,39 @@ Unicorn Tears
 """
 
 coustmerList = {
-    "Wings":0,
-    "Cookies":0,
-    "Spring Rolls":0,
-    "Salmon":0,
-    "Steak":0,
-    "Meat Tornado":0,
-    "A Literal Garden":0,
-    "Ice Cream":0,
-    "Cake":0,
-    "Pie":0,
-    "Coffee":0,
-    "Tea":0,
-    "Unicorn Tears":0
+    "Wings": 0,
+    "Cookies": 0,
+    "Spring Rolls": 0,
+    "Salmon": 0,
+    "Steak": 0,
+    "Meat Tornado": 0,
+    "A Literal Garden": 0,
+    "Ice Cream": 0,
+    "Cake": 0,
+    "Pie": 0,
+    "Coffee": 0,
+    "Tea": 0,
+    "Unicorn Tears": 0
 }
+
 
 def menuOrder():
     print(menu)
     while (True):
-        addIteam = input("would you like to add anything else?  ").capitalize()
         print("**you can Quit anytime by type: exit or x**")
+        addIteam = input("would you like to add anything else?  ").capitalize()
 
         if addIteam in coustmerList:
             coustmerList[addIteam] += 1
             for i in coustmerList:
-                if int(coustmerList[i])>0:
+                if int(coustmerList[i]) > 0:
                     print(f"{coustmerList[i]} item of {i}")
+
+        if (addIteam == "Exit" or addIteam == "X" or addIteam == "Quit"):
+            exit("Good Bye")
 
         if addIteam not in coustmerList:
             print("we dont have this in our menu")
             print(f"Our mune list is: {listMune}")
-
-        if (addIteam == "exit" or addIteam == "x"):
-            exit("Good Bye")
+            
 menuOrder()
